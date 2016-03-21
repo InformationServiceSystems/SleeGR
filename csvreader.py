7a7acaceaed51d5a930e7b5ddb8111a21baa7340
Matthias Heerde <s9maheer@stud.uni-saarland.de>
2016-03-21 13:04:37 +0100

    changed path

--- csvreader.py
+++ csvreader.py
@@ -17,25 +17,43 @@ class csvReader:
     def read_data(self, user_id, start_date, end_date, measurement_type):
         ret_json = {}
 
-        for day in rrule.rrule(rrule.DAILY, dtstart=start_date, until=end_date):
+        for day in rrule.rrule(rrule.DAILY, dtstart=start_date,
+                               until=end_date):
             try:
-                file_name = ('%s/%s/%s-%s.csv' % (self.folder_path, user_id, day.strftime('%Y.%m.%d'), measurement_type))
+                file_name = ('%s/%s/%s-%s.csv' % (
+                self.folder_path, user_id, day.strftime('%Y.%m.%d'),
+                measurement_type))
                 with open(file_name, newline='') as csv_file:
-                    fieldnames = ['UserID', 'measurement_type', 'time_stamp', 'Type_of_activity', 'value_1', 'value_2', 'value_3']
-                    csv_reader = csv.DictReader(csv_file, fieldnames=fieldnames)
+                    fieldnames = ['UserID', 'measurement_type', 'time_stamp',
+                                  'Type_of_activity', 'value_1', 'value_2',
+                                  'value_3']
+                    csv_reader = csv.DictReader(csv_file,
+                                                fieldnames=fieldnames)
                     for row in csv_reader:
-                        if measurement_to_valuenumb[int(row['measurement_type'])] == 1:
-                            ret_json[row['time_stamp']] = {'value' : row['value_1']}
-                        elif measurement_to_valuenumb[int(row['measurement_type'])] == 2:
-                            ret_json[row['time_stamp']] = {'value' : {'value_1': row['value_1'], 'value_2': row['value_2']}}
-                        elif measurement_to_valuenumb[int(row['measurement_type'])] == 3:
-                            ret_json[row['time_stamp']] = {'value' : {'value_1': row['value_1'], 'value_2': row['value_2'], 'value_3': row['value_3']}}
+                        if measurement_to_valuenumb[
+                            int(row['measurement_type'])] == 1:
+                            ret_json[row['time_stamp']] = {
+                                'value': row['value_1']}
+                        elif measurement_to_valuenumb[
+                            int(row['measurement_type'])] == 2:
+                            ret_json[row['time_stamp']] = {
+                                'value': {'value_1': row['value_1'],
+                                          'value_2': row['value_2']}}
+                        elif measurement_to_valuenumb[
+                            int(row['measurement_type'])] == 3:
+                            ret_json[row['time_stamp']] = {
+                                'value': {'value_1': row['value_1'],
+                                          'value_2': row['value_2'],
+                                          'value_3': row['value_3']}}
                         else:
-                            ret_json[row['time_stamp']] = {'value' : {'value_1': row['value_1'], 'value_2': row['value_2'], 'value_3': row['value_3']}}
+                            ret_json[row['time_stamp']] = {
+                                'value': {'value_1': row['value_1'],
+                                          'value_2': row['value_2'],
+                                          'value_3': row['value_3']}}
 
             except FileNotFoundError:
                 pass
-        #return json.JSONEncoder().encode(ret_json)
+        # return json.JSONEncoder().encode(ret_json)
         return ret_json
 
     def ReadSleepData(self, user_id, start_date, end_date):
@@ -51,19 +69,23 @@ class csvReader:
                     continue
                 if row[0] == 'Id':
                     continue
-                wake_up = datetime.strptime(row[3],'%d. %m. %Y %H:%M')
+                wake_up = datetime.strptime(row[3], '%d. %m. %Y %H:%M')
                 drop_off = datetime.strptime(row[2], '%d. %m. %Y %H:%M')
                 wake_up.date()
-                if wake_up.date() >= start_date.date() and wake_up.date() <= end_date.date():
-                    duration = wake_up - drop_off
-                    deep_sleep = float(row[12]) * 100
-                    hours = int(duration.seconds) / 3600
-                    ret_list.append({'user_id': user_id, 'date': str(wake_up.date()), 'x': float(duration.seconds / 3600), 'y': deep_sleep})
+                if wake_up.date() >= start_date.date():
+                    if wake_up.date() <= end_date.date():
+                        duration = wake_up - drop_off
+                        deep_sleep = float(row[12]) * 100
+                        ret_list.append(
+                            {'user_id': user_id, 'date': str(wake_up.date()),
+                             'x': float(duration.seconds / 3600),
+                             'y': deep_sleep})
         return ret_list
 
-    def heart_rate_sepecial(self,user_id, start_date, end_date):
+    def heart_rate_sepecial(self, user_id, start_date, end_date):
         ret_list = []
-        for day in rrule.rrule(rrule.DAILY, dtstart=start_date, until=end_date):
+        for day in rrule.rrule(rrule.DAILY, dtstart=start_date,
+                               until=end_date):
             heart_rate_json = self.read_data(user_id, day, day, '21')
             fkt_json = self.read_data(user_id, day, day, '32')
             if heart_rate_json is None:
@@ -72,9 +94,9 @@ class csvReader:
             new_json['user_id'] = user_id
             new_json['date'] = day.strftime('%d.%m.%Y')
             if fkt_json is None:
-                    new_json['a'] = 0
-                    new_json['b'] = 0
-                    new_json['c'] = 0
+                new_json['a'] = 0
+                new_json['b'] = 0
+                new_json['c'] = 0
             else:
                 for key in fkt_json:
                     new_json['a'] = fkt_json[key]['value']['value_1']
@@ -85,11 +107,13 @@ class csvReader:
             for measurement in heart_rate_json:
                 if counter >= 300:
                     break
-                datapoints.append({'x': counter * 3, 'y':heart_rate_json[measurement]['value']})
+                datapoints.append({'x': counter * 3,
+                                   'y': heart_rate_json[measurement]['value']})
                 counter += 1
             new_json['datapoints'] = datapoints
             ret_list.append(new_json)
         return ret_list
+
 
 """
 a = csvReader()
@@ -111,7 +135,8 @@ measurement_to_valuenumb = {
 }
 
 s = csvReader()
-#e = s.ReadSleepData('test@test.com', datetime(2016, 1,1), datetime(2016, 3, 31))
-e = s.heart_rate_sepecial('test@test.com', datetime(2016, 1,1), datetime(2016, 3, 31))
+# e = s.ReadSleepData('test@test.com', datetime(2016, 1,1), datetime(2016, 3, 31))
+e = s.heart_rate_sepecial('test@test.com', datetime(2016, 1, 1),
+                          datetime(2016, 3, 31))
 for ee in e:
     print(ee)
