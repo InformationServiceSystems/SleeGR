ca3c97257708f40ae6b03fe6ceff495bcbd590b7
Mirco Pyrtek <s9mipyrt@stud.uni-saarland.de>
2016-09-16 12:03:12 +0200

    changed folder structure of adminLTE

--- webpage/static/js/init.js
+++ webpage/static/js/init.js
@@ -78,6 +78,20 @@ define([], function () {
 
 	}
 
+	init.initLogin = function () {
+
+		require(['jquery', 'icheck', 'bootstrap'], function ($) {
+			$(function () {
+        		$('input').iCheck({
+          			checkboxClass: 'icheckbox_square-blue',
+          			radioClass: 'iradio_square-blue',
+          			increaseArea: '20%' // optional
+				});
+      		});
+		});
+
+	}
+
 	return init;
 });
 
