83fb6bb811250d1c44afd55c3afeb784e2d53611
Mirco Pyrtek <s9mipyrt@stud.uni-saarland.de>
2016-09-19 10:12:11 +0200

    deleted backup files7

--- static_back/app/metrics-graphics/histogram/directive.js
+++
@@ -1,7 +0,0 @@
-var MetricsGraphicsHistogramDirective = function() {
-	return {
-        restrict: 'E',
-        templateUrl: 'templates/histogram/histogram.tpl.html',
-        controller: MetricsGraphicsHistogramCtrl
-    };
-};
