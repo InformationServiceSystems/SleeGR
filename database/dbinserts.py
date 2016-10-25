a1b22e4f9df0ceaf1150ae4fc49e13c63782605c
Matthias Heerde <mail@m-heerde.de>
2016-10-25 13:05:59 +0200

    changed argumenttypes in dbinserts to datawrappers

--- database/dbinserts.py
+++ database/dbinserts.py
@@ -1,7 +1,9 @@
 import names
 from database.database import DbBase
 from databasemodels.models import User
-
+import datawrapper.correl_wrapper as cw
+import datawrapper.measure_wrapper as mw
+import datawrapper.value_wrapper as vw
 
 class DbInserts:
     def __init__(self, database):
@@ -49,26 +51,49 @@ class DbInserts:
         else:
             return None
 
-    def insert_heart_rate_data(self, json):
-        self.db_base._db['heart_rate'].insert_one(json)
+    # def insert_heart_rate_data(self, json):
+    #     self.db_base._db['heart_rate'].insert_one(json)
 
-    def insert_sleep_data(self, json):
-        self.db_base._db['sleep_data'].insert_one(json)
+    # def insert_sleep_data(self, json):
+    #     self.db_base._db['sleep_data'].insert_one(json)
 
-    def inser_fitness_data(self, measurement_type, json):
-        self.db_base._db[str(measurement_type)].insert_one(json)
-
+    # def inser_fitness_data(self, measurement_type, json):
+    #     self.db_base._db[str(measurement_type)].insert_one(json)
 
     def insert_fitness(self, user, json):
+        """
+        Deprecated
+        :param user:
+        :param json:
+        :return:
+        """
         self.db_base._db[user].insert_one(json)
 
     # newer
-    def insert_csv_row(self, user, json):
-        if not self.db_base._db[user].insert_one(json) is None:
+
+    def insert_value(self, user, value_wrapper):
+        return self.db_base._db[user].insert_one(value_wrapper._value_json)
+
+    def insert_csv_row(self, user, value_wrapper):
+        if not self.insert_value(user, value_wrapper) is None:
             return True
         return False
 
-    def insert_csv_row_many(self, user, json_lst):
-        if not self.db_base._db[user].insert_many(json_lst) is None:
+    def insert_csv_row_many(self, user, value_wrapper_list):
+        json_list = [value_json._value_json for value_json in value_wrapper_list]
+        if not self.db_base._db[user].insert_many(json_list) is None:
             return True
         return False
+
+    def insert_correl(self, user, correl_wrapper):
+        self.db_base['%s_data' % (user)].insert(correl_wrapper._correlation_json)
+
+    def insert_measure(self, measure_wrapper):
+        ids = []
+        user = ''
+        for value in measure_wrapper:
+            user = value.email
+            ids.append(self.insert_value(value.email, value).inserted_id)
+        measure_json = dict(measure_wrapper._measuremet_json)
+        measure_json['value_ids'] = ids
+        return self.db_base._db[('%s_measure' % user)].insert_one(measure_json)
