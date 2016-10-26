7ba095a270ec30058c43a87a8e1153cb51b9a5b0
Matthias Heerde <mail@m-heerde.de>
2016-10-26 11:53:41 +0200

    use correl_wrapper-methods instead of dict

--- linear_datascience.py
+++ linear_datascience.py
@@ -3,20 +3,38 @@ Contains code that prepares data to be sent to the
 """
 
 import numpy as np
+import re
 from scipy.optimize import minimize
+from datawrapper.correl_wrapper import CorrelWrapper
 
-def Comp1D(vals, A, B, regr=True, B_next_day=False):
+
+def Comp1D(vals, x_label, y_label, regr=True, B_next_day=False):
+    field_names = {
+        'Day of week': CorrelWrapper.day_of_week,
+        'Sleep length' : CorrelWrapper.sleep_length,
+        'Load': CorrelWrapper.load,
+        'Sleep start': CorrelWrapper.sleep_start,
+        'Sleep end': CorrelWrapper.sleep_end,
+        'Deep sleep': CorrelWrapper.deep_sleep,
+        'Activity A': CorrelWrapper.activity_a,
+        'Activity G': CorrelWrapper.activity_g,
+        'RPE': CorrelWrapper.rpe,
+        'DALDA': CorrelWrapper.dalda}
+
+    x_label_method = field_names[x_label]
+    y_label_method = field_names[y_label]
     x, y = [], []
     for i in range(len(vals)):
 
-        vx = (vals[i][A])
-
+        #vx = (vals[i][x_label])
+        vx = x_label_method(vals[i])
         if B_next_day:
             if i + 1 >= len(vals):
                 continue;
-            vy = (vals[i + 1][B])
+            #vy = (vals[i + 1][y_label])
+            vy = y_label_method(vals[i + 1])
         else:
-            vy = (vals[i][B])
+            vy = y_label_method(vals[i])
 
         if not vx is None and not vy is None:
             x.append(vx)
@@ -36,13 +54,13 @@ def Comp1D(vals, A, B, regr=True, B_next_day=False):
     sol = minimize(obj, p0)
     p = sol.x
 
-    xlabel = A
-    ylabel = B + ", next day" if B_next_day else B
-    title = A + ' -> ' + ylabel
+    xlabel = x_label
+    ylabel = y_label + ", next day" if B_next_day else y_label
+    title = x_label + ' -> ' + ylabel
 
     if not (any(x) and any(y)):
         return None
-    data = x,y
+    data = x, y
 
     xmn, xmx = min(x), max(x)
     ymn, ymx = ln(xmn, p), ln(xmx, p)
@@ -56,7 +74,7 @@ def Comp1D(vals, A, B, regr=True, B_next_day=False):
     r['title'] = title
 
     # raw data, set of pairs [x,y]
-    r['data'] = np.column_stack([x,y]).tolist()
+    r['data'] = np.column_stack([x, y]).tolist()
 
     # points that define the linear function
     r['x0'], r['x1'] = xmn, xmx
