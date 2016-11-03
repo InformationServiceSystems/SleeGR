31d0e88edbd8de43ed593ee029326ce8dd3d827e
Matthias Heerde <mail@m-heerde.de>
2016-11-03 11:42:17 +0100

    introduced typehinting to dbextends

--- database/dbextendend.py
+++ database/dbextendend.py
@@ -1,6 +1,7 @@
 from database.database import DbBase
 import names
 import pymongo
+from typing import List, Optional
 from datetime import datetime
 import bcrypt
 from datawrapper import correl_wrapper, measure_wrapper, value_wrapper
@@ -10,16 +11,16 @@ class DbExtended:
         self.db_base = database
 
     def get_all_users(self):
-        asked_user = self.db_base._general_users_collection.find()
-        if asked_user:
-            return asked_user
+        asked_users = self.db_base._general_users_collection.find()
+        if asked_users:
+            return asked_users
         else:
             return None
 
-    def drop_correl(self, user):
+    def drop_correl(self, user: str):
         self.db_base._db.drop_collection('%s_data' % user)
 
-    def get_user_id(self, email):
+    def get_user_id(self, email: str):
         asked_user = self.db_base._general_users_collection.find_one({names.email: email})
         if asked_user:
             return asked_user['_id']
@@ -64,7 +65,8 @@ class DbExtended:
     #                                                       'type': measurement_type}))
 
     #changed to wrapper, not checked
-    def find_data_tag(self, user, measurement, tag=''):
+    #TODO: type for tag
+    def find_data_tag(self, user: str, measurement: str, tag ='') -> List[Optional[value_wrapper.ValueWrapper]]:
         res_lst = self.db_base._db[user].find({'type': measurement, 'tag': tag}).sort('time_stamp', pymongo.ASCENDING)
         ret_lst = []
         for elem in res_lst:
@@ -73,7 +75,7 @@ class DbExtended:
         return ret_lst
 
     #changed to wrapper, not checked
-    def find_data_user(self, user):
+    def find_data_user(self, user:str) -> List[Optional[value_wrapper.ValueWrapper]]:
         res_lst = self.db_base._db[user].find()#.sort('time_stamp', pymongo.ASCENDING)
         ret_lst = []
         for elem in res_lst:
@@ -82,7 +84,7 @@ class DbExtended:
         return ret_lst
 
     # changed to wrapper, not checked
-    def find_data(self, user, time_stamp, measurement):
+    def find_data(self, user:str , time_stamp:datetime, measurement:str) -> List[Optional[value_wrapper.ValueWrapper]]:
         ret_lst =[]
         for elem in self.db_base._db[user].find({'type': measurement}):
             if elem['time_stamp'].date() == time_stamp.date():
@@ -91,7 +93,7 @@ class DbExtended:
         return ret_lst
 
     # changed to wrapper, not checked
-    def find_data_no_date(self, user, measurement):
+    def find_data_no_date(self, user, measurement)-> List[Optional[value_wrapper.ValueWrapper]]:
         ret_lst = []
         for elem in self.db_base._db[user].find({'type': measurement}):
             del elem['_id']
@@ -99,7 +101,7 @@ class DbExtended:
         return ret_lst
 
     # changed to wrapper, not checked
-    def find_correl_data(self, user):
+    def find_correl_data(self, user: str) -> List[correl_wrapper.CorrelWrapper]:
         collection_name = ('%s_data' % user)
         jsons = self.db_base._db[collection_name].find()
         ret_lst = []
@@ -110,12 +112,13 @@ class DbExtended:
         return ret_lst
 
     # changed to wrapper, not checked
-    def find_one_correl_data_date(self, user, time_stamp):
+    def find_one_correl_data_date(self, user: str, time_stamp: datetime)-> Optional[List[correl_wrapper.Correlwrapper]]:
         tmp_lst = []
         for data in self.find_correl_data(user):
             if data.time_stamp == time_stamp.date():
                 tmp_lst.append(data)
         for data in tmp_lst:
+            #TODO: THINK ABOUT THIS IF
             if data.a is not None:
                 return data
         return None
