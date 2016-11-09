cd4555ee12fe3a11ef3e595e73d3c48fc3fe6431
Matthias Heerde <mail@m-heerde.de>
2016-11-09 12:23:05 +0100

    code-cleanup

--- authentification.py
+++ authentification.py
@@ -48,37 +48,30 @@ def authenticate_error(error):
 def requires_auth_api(f):
   @wraps(f)
   def decorated(*args, **kwargs):
-    if 'profile' in session:
-      return f(*args, **kwargs)
-    else:
-        auth = request.headers.get('Authorization', None)
-        if not auth:
-          return authenticate_error({'code': 'authorization_header_missing', 'description': 'Authorization header is expected'})
 
-        parts = auth.split()
-
-        if parts[0].lower() != 'bearer':
-          return authenticate_error({'code': 'invalid_header', 'description': 'Authorization header must start with Bearer'})
-        elif len(parts) == 1:
-          return authenticate_error({'code': 'invalid_header', 'description': 'Token not found'})
-        elif len(parts) > 2:
-          return authenticate_error({'code': 'invalid_header', 'description': 'Authorization header must be Bearer + \s + token'})
-
-        token = parts[1]
-        try:
-            payload = jwt.decode(
-                token,
-                base64.b64decode(client_secret.replace("_","/").replace("-","+")),
-                audience=client_id
-            )
-
-        except jwt.ExpiredSignature:
-            return authenticate_error({'code': 'token_expired', 'description': 'token is expired'})
-        except jwt.InvalidAudienceError:
-            return authenticate_error({'code': 'invalid_audience', 'description': 'incorrect audience, expected: ' + client_id})
-        except jwt.DecodeError:
-            return authenticate_error({'code': 'token_invalid_signature', 'description': 'token signature is invalid'})
-
-        _request_ctx_stack.top.current_user = user = payload
-        return f(*args, **kwargs)
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+      return authenticate_error({'code': 'authorization_header_missing', 'description': 'Authorization header is expected'})
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+      return authenticate_error({'code': 'invalid_header', 'description': 'Authorization header must start with Bearer'})
+    elif len(parts) == 1:
+      return authenticate_error({'code': 'invalid_header', 'description': 'Token not found'})
+    elif len(parts) > 2:
+      return authenticate_error({'code': 'invalid_header', 'description': 'Authorization header must be Bearer + \s + token'})
+    token = parts[1]
+    try:
+        payload = jwt.decode(
+            token,
+            base64.b64decode(client_secret.replace("_","/").replace("-","+")),
+            audience=client_id
+        )
+    except jwt.ExpiredSignature:
+        return authenticate_error({'code': 'token_expired', 'description': 'token is expired'})
+    except jwt.InvalidAudienceError:
+        return authenticate_error({'code': 'invalid_audience', 'description': 'incorrect audience, expected: ' + client_id})
+    except jwt.DecodeError:
+        return authenticate_error({'code': 'token_invalid_signature', 'description': 'token signature is invalid'})
+    _request_ctx_stack.top.current_user = user = payload
+    return f(*args, **kwargs)
   return decorated
